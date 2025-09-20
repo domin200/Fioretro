d9a094cf62281cb1c025efac7f949488c69470aa
domin200 <somakotic@gmail.com>
2025-09-20 15:14:33 +0900

    🎮 Electron 래핑 초기 설정: main.js 생성 및 npm 스크립트 구성

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      webSecurity: true
+    },
+    icon: path.join(__dirname, 'boss', 'boss1.png')
+  });
+
+  mainWindow.loadFile('hwatu.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
